4947ec6bd6811c71cea5f674663866fbe5936da8
Nikolcho Velkovski <kango666@gmail.com>
2025-11-11 21:32:58 +0100

    feat: add blank page
    Create a new blank page component in the pages directory

---
+++ pages/blank.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function BlankPage() {
+ return (
+<div className="p-4">
+<h1 className="text-2xl font-bold">Blank Page</h1>
+<p>This is a new blank page created for demonstration purposes</p>
+</div>
+ );
+}
